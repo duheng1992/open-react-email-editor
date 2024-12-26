642bf70b519da152ce43d98b9cfee21e23c584a2
heng.du <heng.du@ucloud.cn>
2024-12-26 16:44:11 +0800

    fix: set deploy base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/open-react-email-editor/',
   plugins: [react()],
   define: {
     'process.env': {}
